36c14ab635031a756ae2747f5a520ce2f7410ff9
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-03-14 14:44:39 +0100

    Añadida "s"

--- javascipt/main.js
+++ javascipt/main.js
@@ -4,7 +4,7 @@ function main() {
     var resultado = ""
     var repetir = true
     var contador
-    var casillas = document.getElementsByClassName("casilla")
+    var casillas = document.getElementsByClassName("casillas")
     var contador = 0
     var tablero = Inicia_tablero(3, 3)
     for (let i = 0; i < casillas.length; i++) {
