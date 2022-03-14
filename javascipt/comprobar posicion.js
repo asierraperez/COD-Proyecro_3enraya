c3f0824ee5a2541a9aa76f026876a9f4ae7f485e
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-03-14 14:38:30 +0100

    Crear etiqueta img en js y añadir svg

--- javascipt/comprobar posicion.js
+++ javascipt/comprobar posicion.js
@@ -1,11 +1,20 @@
-function comprobar_posicion(i, j, jugador, aux_tablero) {
+function comprobar_posicion(i, j, jugador, aux_tablero, posicion) {
     var aux_comprobar_tablero = aux_tablero
     if (aux_comprobar_tablero[i][j] == "-") {
+        var imagen = document.createElement('img')
         if (jugador == 1) {
             aux_comprobar_tablero[i][j] = "X"
+            imagen.src = "svg/cruz.svg"
+
         } else {
             aux_comprobar_tablero[i][j] = "O"
+            imagen.src = "svg/circulo.svg"
+
         }
+
+        document.getElementsByClassName("casillas")[posicion].appendChild(imagen)
+
+
     } else {
         alert("Jugador " + jugador + "\nPosicion ocupada, intentalo de nuevo")
     }
